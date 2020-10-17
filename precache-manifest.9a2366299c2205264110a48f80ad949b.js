64cb40eee47e6e89738172d478338cfd55da40f7
MeetWadiwala <71484359+MeetWadiwala@users.noreply.github.com>
2020-10-17 13:50:03 +0530

    Updates

---
+++ precache-manifest.9a2366299c2205264110a48f80ad949b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "043a5f7f24150683d7eb7b6bb44482fd",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "ff690b2dfd6ab1f19f63",
+    "url": "/todolist/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5c70579b11ee35cfe5c0",
+    "url": "/todolist/static/css/main.1a785928.chunk.css"
+  },
+  {
+    "revision": "ff690b2dfd6ab1f19f63",
+    "url": "/todolist/static/js/2.f105dbfd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todolist/static/js/2.f105dbfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c70579b11ee35cfe5c0",
+    "url": "/todolist/static/js/main.feb8dc82.chunk.js"
+  },
+  {
+    "revision": "197f51c8578d731c6831",
+    "url": "/todolist/static/js/runtime-main.e7725399.js"
+  }
+]);
